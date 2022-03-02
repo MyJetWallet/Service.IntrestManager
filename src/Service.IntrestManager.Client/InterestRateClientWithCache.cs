9e92b99983b653d23a722a39355c25686318315b
Alexey Novichikhin <alexey.n@smplt.net>
2022-03-02 17:22:12 +0200

    add logs

--- src/Service.IntrestManager.Client/InterestRateClientWithCache.cs
+++ src/Service.IntrestManager.Client/InterestRateClientWithCache.cs
@@ -22,16 +22,25 @@ namespace Service.IntrestManager.Client
 
         public async Task<InterestRateByWallet> GetInterestRatesByWalletAsync(string walletId)
         {
-            var cache = _reader.Get(walletId).FirstOrDefault();
-            if (cache != null && cache.Rates.RateCollection.All(t=>t.NextPaymentDate > DateTime.UtcNow))
-                return cache.Rates;
+            Console.WriteLine($"GetInterestRatesByWalletAsync start ({walletId}) {DateTime.UtcNow:O}");
+            try
+            {
+                var cache = _reader.Get(InterestRateByWalletNoSql.GeneratePartitionKey(walletId)).FirstOrDefault();
+                if (cache != null && cache.Rates.RateCollection.All(t=>t.NextPaymentDate > DateTime.UtcNow))
+                    return cache.Rates;
 
-            var ratesFromGrpc = await _interestRateClientService.GetInterestRatesByWalletAsync(
-                new GetInterestRatesByWalletRequest()
-                {
-                    WalletId = walletId
-                });
-            return ratesFromGrpc.Rates;
+                var ratesFromGrpc = await _interestRateClientService.GetInterestRatesByWalletAsync(
+                    new GetInterestRatesByWalletRequest()
+                    {
+                        WalletId = walletId
+                    });
+                return ratesFromGrpc.Rates;
+            }
+            finally
+            {
+                Console.WriteLine($"GetInterestRatesByWalletAsync stop ({walletId}) {DateTime.UtcNow:O}");
+            }
+            
         }
     }
 }
